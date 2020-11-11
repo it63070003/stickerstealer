083e300d3e546a50c9e147dfac658cef01653c38
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-11-11 17:02:14 +0700

    Sticker Stealer WIP2
    Added URL input
    Get all Stickers URL

--- StickerStealer.py
+++ StickerStealer.py
@@ -1,13 +1,21 @@
 """Sticker Stealer"""
 import requests
+from bs4 import BeautifulSoup
 
 
 def main():
     """MainFunc"""
-    url = input()
-    web_code = requests.get(url)
-    print(web_code.content)
-
+    #url = input()
+    link = input("Insert Sticker URL:")
+    web_code = requests.get(link).content
+    soup = BeautifulSoup(web_code, "html.parser")
+    url_list = list(soup.find_all("span", class_="mdCMN09Image FnPreview"))
+    for i in range(len(url_list)):
+        url_list[i] = str(url_list[i])
+        start = url_list[i].index("url") + 4
+        stop = url_list[i].index(";")
+        url_list[i] = url_list[i][start:stop]
+    print(url_list)
 
 
 main()
