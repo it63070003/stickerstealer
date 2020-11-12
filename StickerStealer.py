8023b6abf93dd828afed65b4f5b1e6251b693ac9
it63070003 <73647989+it63070003@users.noreply.github.com>
2020-11-12 15:40:29 +0700

    Update StickerStealer.py
    Added Comments

--- StickerStealer.py
+++ StickerStealer.py
@@ -5,17 +5,19 @@ from bs4 import BeautifulSoup
 
 def main():
     """MainFunc"""
-    #url = input()
+    #Get Website Code
     link = input("Insert Sticker URL:")
     web_code = requests.get(link).content
     soup = BeautifulSoup(web_code, "html.parser")
+    #Find all Sticker Images
     url_list = list(soup.find_all("span", class_="mdCMN09Image FnPreview"))
     for i in range(len(url_list)):
         url_list[i] = str(url_list[i])
         start = url_list[i].index("url") + 4
         stop = url_list[i].index(";")
         url_list[i] = url_list[i][start:stop]
-    print(url_list)
+    #url_list : Sticker Image URLs
+    #Download Every Images
 
 
 main()
